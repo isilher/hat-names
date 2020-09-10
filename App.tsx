c4270f26c8536197cc62658dae54d7aa447d166d
Rik <ik@rikvanduijn.nl>
2020-09-11 00:01:59 +0200

    Tweak settings

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import Amplify, { withAuthenticator } from 'aws-amplify-react-native'
+import { withAuthenticator } from 'aws-amplify-react-native'
+import Amplify from 'aws-amplify'
 // @ts-expect-error aws exports has no typing
 import awsconfig from './aws-exports.js'
 
